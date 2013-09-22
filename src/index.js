e1fa536f62db84b89439239b7c3fc8d8da7e895d
jerome creignou <jerome.creignou@gmail.com>
2013-09-22 19:33:36 +0200

    add first impm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*jslint node: true, vars: true, indent: 4 */
+
+'use strict';
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.write('NOTHING');
+    res.end();
+}).listen(process.env.PORT || 8080);
