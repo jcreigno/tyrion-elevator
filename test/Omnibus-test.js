697b1acbc0cf7c03a92ba5428a46b0537b062582
jerome creignou <jerome.creignou@gmail.com>
2013-09-24 11:05:21 +0200

    add tests

--- test/Omnibus-test.js
+++ test/Omnibus-test.js
@@ -5,12 +5,41 @@ var Omnibus = require('../src/Omnibus'),
     vows = require('vows'),
     assert = require('assert');
 
-vows.describe('Omnibus Elevator').addBatch({
-    'peut etre instancié':{
-        topic: new Omnibus(5),
-        ' le cycle est de 5x2x3 étapes': function (o) {
-            //console.log(o.cycle);
-            assert.equal(o.cycle.length, 5*2*3);
+var upward = Array.apply(null, new Array(5)).map(function () {
+    return ['UP', 'OPEN', 'CLOSE'];
+});
+var downward = Array.apply(null, new Array(5)).map(function () {
+    return ['DOWN', 'OPEN', 'CLOSE'];
+});
+
+function appendCommands(batch){
+    var context = batch['peut etre instancié'];
+    var cmd = [].concat.apply([], upward.concat(downward)); //flatten
+    cmd = cmd.concat(cmd);
+    cmd.forEach(function (e, i){
+        var subtopic = context['on appelle la commande ' + (i + 1)] = {
+            topic : function (o) {
+                return o.nextCommand();
+            }
+        };
+        subtopic['on obtient "' + e + '"'] = function (res) {
+            assert.equal(res, e);
+        };
+    });
+    return batch;
+}
+
+function createBatch() {
+    var result = {
+        'peut etre instancié': {
+            topic: new Omnibus(5),
+            ' le cycle est de 5x2x3 étapes': function (o) {
+                //console.log(o.cycle);
+                assert.equal(o.cycle.length, 5 * 2 * 3);
+            }
         }
-    }
-}).export(module);
+    };
+    return  appendCommands(result);
+}
+
+vows.describe('Omnibus Elevator').addBatch(createBatch()).export(module);
