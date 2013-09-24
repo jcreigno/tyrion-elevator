cbca4d9505bdc9299eb4becafb1155f4d5435ceb
jerome creignou <jerome.creignou@gmail.com>
2013-09-24 15:00:37 +0200

    use underscore for omnibus

--- src/Omnibus.js
+++ src/Omnibus.js
@@ -1,20 +1,41 @@
-/*jslint node: true, vars: true, indent: 4, plusplus: true */
-
+/*jslint node: true, vars: true, indent: 4, plusplus: true, nomen: true*/
 'use strict';
+var _ = require('underscore');
+
 
 function Omnibus(size) {
-    var upward = Array.apply(null, new Array(size)).map(function () {
+    var upward = _(size).times(function () {
         return ['UP', 'OPEN', 'CLOSE'];
     });
-    var downward = Array.apply(null, new Array(size)).map(function () {
+    var downward = _(size).times(function () {
         return ['DOWN', 'OPEN', 'CLOSE'];
     });
-    this.cycle = [].concat.apply([], upward.concat(downward)); //flatten
+    this.cycle = _.flatten(upward.concat(downward));
     this.current = 0;
 }
 
 Omnibus.prototype.nextCommand = function () {
     return this.cycle[(this.current++) % this.cycle.length];
+};
+
+Omnibus.prototype.call = function (atFloor, to) {
+    console.log('call at floor %d to %s', atFloor, to);
+};
+
+Omnibus.prototype.go = function (floorToGo) {
+    console.log('go to floor %d', floorToGo);
+};
+
+Omnibus.prototype.userHasEntered = function () {
+    console.log('user has entered');
+};
+
+Omnibus.prototype.userHasExited = function () {
+    console.log('user has exited');
+};
+
+Omnibus.prototype.reset = function (cause) {
+    console.log('reset : %s', cause);
 };
 
 module.exports = function (size) {
--- test/Omnibus-test.js
+++ test/Omnibus-test.js
@@ -1,22 +1,23 @@
-/*jslint node: true, vars: true, indent: 4 */
+/*jslint node: true, vars: true, indent: 4, nomen:true */
 'use strict';
 
 var Omnibus = require('../src/Omnibus'),
     vows = require('vows'),
-    assert = require('assert');
+    assert = require('assert'),
+    _ = require('underscore');
 
-var upward = Array.apply(null, new Array(5)).map(function () {
+var upward = _(5).times(function () {
     return ['UP', 'OPEN', 'CLOSE'];
 });
-var downward = Array.apply(null, new Array(5)).map(function () {
+var downward = _(5).times(function () {
     return ['DOWN', 'OPEN', 'CLOSE'];
 });
 
-function appendCommands(batch){
+function appendCommands(batch) {
     var context = batch['peut etre instancié'];
-    var cmd = [].concat.apply([], upward.concat(downward)); //flatten
+    var cmd = _.flatten(upward.concat(downward));
     cmd = cmd.concat(cmd);
-    cmd.forEach(function (e, i){
+    cmd.forEach(function (e, i) {
         var subtopic = context['on appelle la commande ' + (i + 1)] = {
             topic : function (o) {
                 return o.nextCommand();
@@ -36,10 +37,17 @@ function createBatch() {
             ' le cycle est de 5x2x3 étapes': function (o) {
                 //console.log(o.cycle);
                 assert.equal(o.cycle.length, 5 * 2 * 3);
+            },
+            ' l\'objet contient les méthodes requises': function (o) {
+                assert.ok(o.nextCommand);
+                assert.ok(o.userHasEntered);
+                assert.ok(o.userHasExited);
+                assert.ok(o.call);
+                assert.ok(o.go);
             }
         }
     };
-    return  appendCommands(result);
+    return appendCommands(result);
 }
 
 vows.describe('Omnibus Elevator').addBatch(createBatch()).export(module);
